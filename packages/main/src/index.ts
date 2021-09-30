eeb957e95709c4ff42fac509a4cd5b0308c20970
mrslimslim <2451319596@qq.com>
2021-09-30 17:47:09 +0800

    add file

---
+++ packages/main/src/index.ts
@@ -0,0 +1,121 @@
+import {app, BrowserWindow, ipcMain, dialog, shell} from 'electron';
+import {join} from 'path';
+import {URL} from 'url';
+const fs = require('fs');
+const simpleGit = require('simple-git');
+
+const isSingleInstance = app.requestSingleInstanceLock();
+
+if (!isSingleInstance) {
+  app.quit();
+  process.exit(0);
+}
+
+app.disableHardwareAcceleration();
+
+// Install "Vue.js devtools"
+if (import.meta.env.MODE === 'development') {
+  app.whenReady()
+    .then(() => import('electron-devtools-installer'))
+    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
+      loadExtensionOptions: {
+        allowFileAccess: true,
+      },
+    }))
+    .catch(e => console.error('Failed install extension:', e));
+}
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = async () => {
+  mainWindow = new BrowserWindow({
+    show: false, // Use 'ready-to-show' event to show window
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation:false,
+      nativeWindowOpen: true,
+      preload: join(__dirname, '../../preload/dist/index.cjs'),
+    },
+  });
+
+  /**
+   * If you install `show: true` then it can cause issues when trying to close the window.
+   * Use `show: false` and listener events `ready-to-show` to fix these issues.
+   *
+   * @see https://github.com/electron/electron/issues/25012
+   */
+  mainWindow.on('ready-to-show', () => {
+    mainWindow?.show();
+
+    if (import.meta.env.MODE === 'development') {
+      mainWindow?.webContents.openDevTools();
+    }
+  });
+
+  /**
+   * URL for main window.
+   * Vite dev server for development.
+   * `file://../renderer/index.html` for production and test
+   */
+  const pageUrl = import.meta.env.MODE === 'development' && import.meta.env.VITE_DEV_SERVER_URL !== undefined
+    ? import.meta.env.VITE_DEV_SERVER_URL
+    : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
+
+
+  await mainWindow.loadURL(pageUrl);
+};
+
+
+app.on('second-instance', () => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
+  }
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.handle('openDialog', async ()=>{
+  const result = await dialog.showOpenDialog({ properties: ['openDirectory'] })
+  return result;
+})
+
+ipcMain.handle('writeFile', async (event, path)=>{
+  console.log('path',path)
+  fs.writeFileSync(path+'/text.txt','something')
+})
+
+ipcMain.handle('showInFinder', async (event, path)=>{
+  shell.showItemInFolder(path);
+})
+
+ipcMain.handle('getGitInfo', async (event, path)=>{
+  console.log('path', path);
+  const git = simpleGit(path, { binary: 'git' });
+  // const config = git.getConfig('user.name', 'global');
+  // const branches = git.branchLocal();
+  const config = git.listConfig('local');
+  // const url = config['values']['.git/config']['remote.origin.url'];
+  return config;
+})
+
+
+app.whenReady()
+  .then(createWindow)
+  .catch((e) => console.error('Failed create window:', e));
+
+
+// Auto-updates
+if (import.meta.env.PROD) {
+  app.whenReady()
+    .then(() => import('electron-updater'))
+    .then(({autoUpdater}) => autoUpdater.checkForUpdatesAndNotify())
+    .catch((e) => console.error('Failed check updates:', e));
+}
+
---
+++ packages/renderer/src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home-page">
+     <div><button @click="handleChoosePath">选择路径</button></div>
+     <div>当前路径: {{dirPath}}</div>
+     <div>创建项目名称 <input @change="handleChangeProjectName" type="text"></div>
+     <div>
+       <button @click="createProject">writeSomething</button>
+     </div>
+     <div>
+       <button @click="showFinder">show Finder</button>
+     </div>
+     <button @click="getGitInfo">获取GIT信息</button>
+     <div class="git-info">
+       <div>
+          git信息展示
+       </div>
+     </div>
+  </div>
+</template>
+
+<script lang="ts">
+const { ipcRenderer } = require("electron");
+import {defineComponent, ref} from 'vue';
+export default defineComponent({
+  name: 'Home',
+  setup() {
+  },
+  data(){
+    return {
+      dirPath: '',
+      projectName: ''
+    };
+  },
+  methods:{
+    async handleChoosePath(){
+      const res = await ipcRenderer.invoke('openDialog');
+      this.dirPath = res.filePaths[0];
+    },
+    handleChangeProjectName(e: any){
+      console.log(e.target.value)
+      this.projectName = e.target.value;
+    },
+    async createProject(){
+       await ipcRenderer.invoke('writeFile', this.dirPath);
+      // fs.writeFileSync(this.dirPath+'/text.txt','something')
+    },
+    async showFinder(){
+      await ipcRenderer.invoke('showInFinder', this.dirPath);
+    },
+    async getGitInfo(){
+      const ret = await ipcRenderer.invoke('getGitInfo', this.dirPath);
+      console.log(ret);
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.home-page{
+  color: red;
+}
+</style>
